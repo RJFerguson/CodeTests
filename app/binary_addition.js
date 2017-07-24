3c93ac2c4d612ddd76317e6bc64993055ec3bce6
Ryan Ferguson <fergusman@gmail.com>
2017-07-24 17:50:27 -0400

    added binary addition

---
+++ app/binary_addition.js
@@ -0,0 +1,7 @@
+var exports = module.exports={};
+
+exports.addBinary = function(a,b){
+  var check = (a + b).toString(2)
+  
+  return check
+};
---
+++ spec/binary_addition.spec.js
@@ -0,0 +1,17 @@
+var app = require("../app/binary_addition.js");
+
+describe("it should add two numbers together and convert it to binary", function(){
+  describe("it should add two numbers together", function(){
+    it('should add two numbers together', function(){
+      var total = app.addBinary(1,5)
+      expect(total).not.toBeNull()
+    });
+  });
+  describe("it should convert the number to binary", function(){
+    it('should convert number to binary', function(){
+      var total = app.addBinary(1,5)
+      expect(total).toMatch('110')
+    });
+  });
+
+});
