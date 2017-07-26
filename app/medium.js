e3f7ce8945bfd67ee474251a2bc8be3f7e57f87a
Ryan Ferguson <fergusman@gmail.com>
2017-07-26 15:26:26 -0400

    added a medium level problem

--- app/medium.js
+++ app/medium.js
@@ -4,3 +4,40 @@ exports.addBinary = function(a,b){
   var check = (a + b).toString(2)
   return check
 };
+
+exports.missingArray = function(arrayOfArrays){
+  var check = [] 
+  var missingNum = 0; 
+  var nullCheck;
+  if(arrayOfArrays === null || arrayOfArrays.length <= 0){
+    return 0
+  } 
+  arrayOfArrays.forEach(function(item){
+    if (item === null){
+     nullCheck = 0; 
+    } 
+  })
+  if (nullCheck === 0){
+    return nullCheck
+  }
+  arrayOfArrays.forEach(function(item){
+    check.push(item.length)
+  })
+  check.sort(function(a,b) {return a-b})
+ 
+  if( check[0] === 0){
+    return 0 
+  }else {
+    for(let i = check[check.length-1]; i > check[0]; i-- ){
+    if (check.indexOf(i) === -1){
+      missingNum = i 
+    }
+  }
+  }
+  if (nullCheck === 0){
+    return nullCheck
+  }
+  else {
+    return missingNum
+  }
+  }
--- spec/medium.spec.js
+++ spec/medium.spec.js
@@ -14,3 +14,33 @@ describe("it should add two numbers together and convert it to binary", function
     });
   });
 });
+
+describe("find the size of the required missing array", function(){
+  describe("it should return 0 if anything is null, undefined, or empty", function(){
+    it('should return 0 if a null value exists', function(){
+      var result = app.missingArray([ [ 1, 2, 2 ], null ])
+      expect(result).toBe(0)
+    });
+    it('should return 0 of a array is empty', function(){
+      var result = app.missingArray([ [ 4 ], [], [ 0, 3, 2, 1 ], [ 2, 2, 4 ] ])
+      expect(result).toBe(0)
+    });
+  });
+  describe("it should return the length of the array missing", function(){
+    it('should return the correct array value', function(){
+      var result = app.missingArray([ [ 4, 3, 0, 3 ],
+  [ 1, 0, 1, 2, 3 ],
+  [ 3, 4, 3 ],
+  [ 1, 2, 3, 3, 2, 0 ],
+  [ 0, 1, 0, 4, 2, 2, 4 ],
+  [ 2, 0, 1, 4, 3, 1, 3, 3 ],
+  [ 1, 1, 2, 2, 2, 0, 4, 3, 4, 3 ],
+  [ 3, 4, 0, 0, 1, 0, 0, 3, 1, 0, 3 ] ])
+      expect(result).toBe(9)
+    });
+    it('should return 0 of a array is empty', function(){
+      var result = app.missingArray([ [ 1, 2 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ] ])
+      expect(result).toBe(3)
+    });
+  });
+});
